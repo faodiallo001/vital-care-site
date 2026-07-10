fd35b3df41ebb9bdf4e931f179a18c61a7ca4e88
faodiallo001 <faodiallo001@gmail.com>
2026-07-10 12:16:43 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -63,3 +63,10 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 });
+
+const menuToggle = document.getElementById("menuToggle");
+const mobileMenu = document.getElementById("mobileMenu");
+
+menuToggle.addEventListener("click", () => {
+    mobileMenu.classList.toggle("active");
+});
